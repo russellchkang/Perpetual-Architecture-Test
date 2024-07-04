31f35534e780f328cdac6dac1758362428181ee5
kimchankwon <kimchankwon@gmail.com>
2024-07-04 22:26:28 +1000

    Works in azure

--- ui/app/discover/page.tsx
+++ ui/app/discover/page.tsx
@@ -1,5 +1,20 @@
-const Page = () => {
-  return <div>page</div>;
+import ChatWindow from '@/components/ChatWindow';
+import { Metadata } from 'next';
+import { Suspense } from 'react';
+
+export const metadata: Metadata = {
+  title: 'Perpetual AI',
+  description: 'AI powered chatbot connected to UNSW resource database',
 };
 
-export default Page;
+const Home = () => {
+  return (
+    <div>
+      <Suspense>
+        <ChatWindow />
+      </Suspense>
+    </div>
+  );
+};
+
+export default Home;
